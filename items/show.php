a996aa50a5383fd9ba0172594997e176bd554d48
CG <cmg100@pitt.edu>
2018-11-12 13:22:50 +0100

    Update show.php
    Updates metadata display

--- items/show.php
+++ items/show.php
@@ -1,91 +1,93 @@
+<?php echo head(array('title' => metadata('item', array('Dublin Core', 'Title')),'bodyclass' => 'item show')); ?>
+
 <?php
-$title = metadata('item', 'display_title');
-$itemFiles = $item->Files;
-$images = array();
-$nonImages = array();
-foreach ($itemFiles as $itemFile) {
-    $mimeType = $itemFile->mime_type;
-    if (strpos($mimeType, 'image') !== false) {
-        $images[] = $itemFile;
+  $title = metadata('item', array('Dublin Core', 'Title'));
+  $subject = metadata('item', array('Dublin Core', 'Subject'));
+  $description = metadata('item', array('Dublin Core', 'Description'));
+  $creators = metadata('item', array('Dublin Core', 'Creator'));
+  $source = metadata('item', array('Dublin Core', 'Source'));
+  $publisher = metadata('item', array('Dublin Core', 'Publisher'));
+  $date = metadata('item', array('Dublin Core', 'Date'));
+  $contributors = metadata('item', array('Dublin Core', 'Contributor'));
+  $rights = metadata('item', array('Dublin Core', 'Rights'));
+  $relation = metadata('item', array('Dublin Core', 'Relation'));
+  $format = metadata('item', array('Dublin Core', 'Format'));
+  $language = metadata('item', array('Dublin Core', 'Language'));
+  $type = metadata('item', array('Dublin Core', 'Type'));
+  $identifier = metadata('item', array('Dublin Core', 'Identifier'));
+  $coverage = metadata('item', array('Dublin Core', 'Coverage'));
+  $tags = tag_string('item', 'items/browse', '');
+  $citation = metadata('item', 'citation', array('no_escape' => true));
+  $collection = link_to_collection_for_item();
+  $outputFormat = output_format_list(false, '');
+
+  function showItemDescriptionTag($tagName, $tagVal) {
+    echo __('<div class="item-description-tag">');
+    echo __('  <h1>'.$tagName.'</h1>');
+    if ($tagName == 'TITLE') {
+      echo __('  <b>'.$tagVal.'</b>');
     } else {
-        $nonImages[] = $itemFile;
+      echo __('  <p>'.$tagVal.'</p>');
     }
-}
-$hasImages = (count($images) > 0);
-if ($hasImages) {
-    queue_css_file('chocolat');
-    queue_js_file('modernizr', 'javascripts/vendor');
-    queue_js_file('jquery.chocolat.min', 'js');
-    queue_js_file('items-show', 'js');
-}
-echo head(array('title' => $title, 'bodyclass' => 'items show' .  (($hasImages) ? ' gallery' : '')));
+    echo __('</div>');
+  }
 ?>
+<div class="container item">
+  <div class="section-header col-md-8 col-md-offset-2">
+    <small>-ITEM-</small>
+    <h1><?php echo $title ?></h1>
+  </div><!-- end of section-header -->
 
-<div class="flex">
-<!-- The following returns all of the files associated with an item. -->
-<?php if ($hasImages): ?>
-    <div id="itemfiles" <?php echo (count($images) == 1) ? 'class="solo"' : ''; ?>>
-        <div id="itemfiles-stage"></div>
-        <div id="itemfiles-nav">
-            <?php foreach ($images as $image): ?>
-                <a href="<?php echo $image->getWebPath('original'); ?>" class="chocolat-image">
-                    <?php echo file_image('square_thumbnail', array(), $image); ?>
-                </a>
-            <?php endforeach; ?>
-        </div>
+
+      
+  <?php 
+    if (get_theme_option('Item FileGallery') == 0 && metadata('item', 'has files')) {
+      if (metadata('item', 'file_count') > 1) {
+        echo __('<div class="multi-item-files col-lg-8 col-lg-offset-2 col-md-12">');
+        echo files_for_item(array('imageSize' => 'square_thumbnail'), array('class' => 'item-file col-md-6'));
+      } else {
+        echo __('<div class="single-item-files col-lg-8 col-lg-offset-2 col-md-12">');
+        echo files_for_item(array('imageSize' => 'fullsize'));
+      }
+      echo __('</div><!-- end of item-files -->'); 
+    }
+  
+  ?>
+  
+
+  <div class="item-description col-lg-12">
+    <div class="col-lg-2 col-md-3 col-sm-6 col-lg-offset-2">
+      <?php 
+        showItemDescriptionTag('NAME', $title); 
+    	showItemDescriptionTag('CONTENTS', $subject);
+
+      ?>
     </div>
-<?php endif; ?>
 
-<div class="item-metadata">
-    <nav>
-    <ul class="item-pagination navigation">
-        <li id="previous-item" class="previous"><?php echo link_to_previous_item_show(); ?></li>
-        <li id="next-item" class="next"><?php echo link_to_next_item_show(); ?></li>
-    </ul>
-    </nav>
-
-    <h1><?php echo metadata('item', 'display_title'); ?></h1>
-
-    <?php echo all_element_texts('item'); ?>
-    
-    <?php if (metadata('item', 'Collection Name')): ?>
-    <div id="collection" class="element">
-        <h3><?php echo __('Collection'); ?></h3>
-        <div class="element-text"><p><?php echo link_to_collection_for_item(); ?></p></div>
-    </div>
-    <?php endif; ?>
-    
-    <!-- The following prints a list of all tags associated with the item -->
-    <?php if (metadata('item', 'has tags')): ?>
-    <div id="item-tags" class="element">
-        <h3><?php echo __('Tags'); ?></h3>
-        <div class="element-text"><?php echo tag_string('item'); ?></div>
-    </div>
-    <?php endif;?>
-
-    <?php if (count($nonImages) > 0): ?>
-    <div id="other-media" class="element">
-        <h3>Other Media</h3>
-        <?php foreach ($nonImages as $nonImage): ?>
-        <div class="element-text"><a href="<?php echo file_display_url($nonImage, 'original'); ?>"><?php echo metadata($nonImage, 'display_title'); ?> - <?php echo $nonImage->mime_type; ?></a></div>
-        <?php endforeach; ?>
-    </div>
-    <?php endif; ?>
-    
-    <!-- The following prints a citation for this item. -->
-    <div id="item-citation" class="element">
-        <h3><?php echo __('Citation'); ?></h3>
-        <div class="element-text"><?php echo metadata('item', 'citation', array('no_escape' => true)); ?></div>
+    <div class="col-lg-2 col-md-3 col-sm-6">
+      <?php 
+        showItemDescriptionTag('LOCATION', $source); 
+        showItemDescriptionTag('PHYSICAL ACCESS', $rights);
+      ?>
     </div>
     
-    <div id="item-output-formats" class="element">
-        <h3><?php echo __('Output Formats'); ?></h3>
-        <div class="element-text"><?php echo output_format_list(); ?></div>
+    <div class="col-lg-2 col-md-3 col-sm-6">
+      <?php 
+         
+        showItemDescriptionTag('ONLINE ACCESS', $format);
+        showItemDescriptionTag('WEBSITE', $publisher); 
+      ?>
+    </div>
+
+    <div class="col-lg-2 col-md-3 col-sm-6">
+      <?php 
+              showItemDescriptionTag('TYPE', $type);
+              showItemDescriptionTag('LANGUAGE', $language);
+
+      ?>
     </div>
     
-    <?php fire_plugin_hook('public_items_show', array('view' => $this, 'item' => $item)); ?>
-</div>
-
-</div>
+  </div><!-- end of item-description -->
+</div><!-- end of item container -->
 
 <?php echo foot(); ?>
